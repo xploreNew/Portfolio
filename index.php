133f99553736d927ef1100cd256c8f2c3abf28e2
kiran <85550311+xploreNew@users.noreply.github.com>
2024-08-07 11:38:24 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,339 @@
+<?php
+//print_r($_POST);
+if($_POST){
+    // the message
+    $msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+//$headers .= 'To: Mary <mary@example.com>, Kelly <kelly@example.com>' . "\r\n";
+$headers .= 'From: Birthday Reminder <copidi5214@biowey.com>' . "\r\n";
+$headers .= 'Cc: copidi5214@biowey.com' . "\r\n";
+$headers .= 'Bcc: copidi5214@biowey.com' . "\r\n";
+
+// send email
+$success = mail("copidi5214@biowey.com","My subject",$msg);
+// if($success){
+//     echo("success");
+// } else {
+//     echo("failure");
+// }
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>My Portfolio</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="libs/font-awesome/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+
+<body>
+    <div id="mobile-menu-open" class="shadow-large">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+    <!-- End #mobile-menu-toggle -->
+    <header>
+        <div id="mobile-menu-close">
+            <span>Close</span> <i class="fa fa-times" aria-hidden="true"></i>
+        </div>
+        <ul id="menu" class="shadow">
+            <li>
+                <a href="#about">Über Mich</a>
+            </li>
+            <li>
+                <a href="#experience">Erfahrung</a>
+            </li>
+            <li>
+                <a href="#education">Education</a>
+            </li>
+            <li>
+                <a href="#projects">Praktikas</a>
+            </li>
+            <li>
+                <a href="#skills">Fähigkeiten</a>
+            </li>
+            <li>
+                <a href="#contact">Kontakt</a>
+            </li>
+        </ul>
+    </header>
+    <!-- End header -->
+
+    <div id="lead">
+        <div id="lead-content">
+            <h1>Kiran Mayi Gangavaram</h1>
+            <h2>Web Developer</h2>
+            <a href="#" class="btn-rounded-white">Download Resume</a>
+        </div>
+        <!-- End #lead-content -->
+
+        <div id="lead-overlay"></div>
+
+        <div id="lead-down">
+            <span>
+                <i class="fa fa-chevron-down" aria-hidden="true"></i>
+            </span>
+        </div>
+        <!-- End #lead-down -->
+    </div>
+    <!-- End #lead -->
+
+    <div id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="heading">Über mich</h2>
+                </div>
+                <div class="col-md-8">
+                    <p>
+                        Als sorgfältiger Softwareentwickler mit 3+ Jahren Erfahrung verfüge ich über exzellente
+                        Fähigkeiten im Bereich Programmierung, Problemlösung. Durch meine Leidenschaft für responsives
+                        Website-Design konnte ich einen neuen Website-Ansatz implementieren.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End #about -->
+
+    <div id="experience" class="background-alt">
+        <h2 class="heading">Erfahrung</h2>
+        <div id="experience-timeline">
+            <div data-date="Dezember 2022 – September 2023">
+                <h3>Invokable</h3>
+                <h4>Web Developer</h4>
+                <p>
+                <ul>
+                    <li>Entwicklung von TYPO3-basierten Projekten im Backend-Bereich</li>
+                    <li>Websites auf die neueste Version upgraden.</li>
+                    <li>Zusammenarbeit mit anderen Teammitgliedern aus den
+                        Unternehmensbereichen Frontend, Projektmanagement und
+                        Support</li>
+                </ul>
+                </p>
+            </div>
+
+            <div data-date="January 2008 – September 2010">
+                <h3>Keane India, Bangalore, Indien</h3>
+                <h4>Software Entwicklerin</h4>
+                <p>
+                <ul>
+                    <li>
+                        MOSS (Microsoft Office Sharepoint Services) 2007 Entwickler für
+                        KMS(Knowledge Management System) Projekt</li>
+                    <li>
+                        Analyse wahrscheinlicher Ansätze mit dem MOSS-Objektmodell</li>
+                    <li>
+                        Implementierung von benutzerdefinierten Webparts</li>
+                    <li>
+                        Im Rahmen des manuellen Testteams war verantwortlich für
+                        logging Fehler und Funktionalitäten identify verpasste
+                        Anforderung specifaction basiert. </li>
+                </ul>
+
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- End #experience -->
+
+    <div id="education">
+        <h2 class="heading">Bildung</h2>
+        <div class="education-block">
+            <h3>Master of Technology – Computer Science & Engineering</h3>
+            <span class="education-date">09.2005 – 07.2007</span>
+            <h4>A.M.C. Engineering College, Bangalore (Indien) </h4>
+            <!-- <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in iaculis ex. Etiam volutpat laoreet urna. Morbi ut tortor nec nulla commodo malesuada sit amet vel lacus. Fusce eget efficitur libero. Morbi dapibus porta quam laoreet placerat.
+            </p> -->
+        </div>
+        <!-- End .education-block -->
+        <div class="education-block">
+            <h3>Bachelor of Engineering – Information Science & Engineering</h3>
+            <span class="education-date">08.2000 – 07.2004</span>
+            <h4>V.E.C. Engineering College, Bellari (Indien) </h4>
+        </div>
+        <!-- End .education-block -->
+        <div class="education-block">
+            <h3>Secondar Schulabschlus</h3>
+            <span class="education-date">06.1988 – 05.2000</span>
+            <h4>S.D.M.C Mysore (Indien) </h4>
+        </div>
+        <!-- End .education-block -->
+    </div>
+    <!-- End #education -->
+     
+    <div id="projects" class="background-alt">
+        <h2 class="heading">Praktika</h2>
+        <div class="container">
+            <div class="row">
+                <div class="project shadow-large">
+                    <div class="project-image">
+                        <img src="images/project.jpg" />
+                    </div>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>Software Entwickler im Rahmen des Master-Studiums 
+                            Hal, Indien 
+                           </h3>
+                        <p>
+                            Programmierung von Wettervorhersagen mit OpenGL
+                        </p>
+                    </div>
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+
+                <div class="project shadow-large">
+                    <div class="project-image">
+                        <img src="images/project.jpg" />
+                    </div>
+                    <!-- End .project-image -->
+                    <div class="project-info">
+                        <h3>Software Entwickler im Rahmen des Bachelor-Studiums, 
+                            BSNL, Indien 
+                            </h3>
+                        <p>
+                           Programmierung HTML in WML
+                           
+                        </p>
+                    </div>
+                    <!-- End .project-info -->
+                </div>
+                <!-- End .project -->
+            </div>
+        </div>
+    </div>
+    <!-- End #projects -->
+
+    <div id="further-training">
+        <h2 class="heading">Weiterbildung</h2>
+        <div class="further-training-block">
+
+            <h3>Deutschkurs C1</h3>
+            <span class="education-date">12.2023 – 06.2024</span>
+            <h4>VHS, Krefeld</h4>
+        </div>
+        <div class="further-training-block">
+            <h3>ISTQB CTFL (Certified Tester Foundation Level)</h3>
+            <span class="education-date">03.2022 – 04.2022</span>
+            <h4>ISTQB Online Portal</h4>
+        </div>
+        <div class="further-training-block">
+            <h3>PHP and MySQL Developer und CMS mit TYPO3</h3>
+            <span class="education-date">04.2021 – 07.2021</span>
+            <h4>Alfatraining, Duisburg</h4>
+        </div>
+        <div class="further-training-block">
+            <h3>Ausbildung im T-SQL, C#, ASP.NET, Microsoft Virtual Tech</h3>
+            <span class="education-date">01.2008 – 06.2008</span>
+            <h4>Keane India GmbH (Indien) </h4>
+
+        </div>
+        <!-- End .education-block -->
+    </div>
+    <!-- End #further-training -->
+
+
+    <div id="skills">
+        <h2 class="heading">Skills</h2>
+        <ul>
+            <li>JavaScript</li>
+            <li>PHP 8</li>
+            <li>MySQL</li>
+            <li>VB</li>
+            <li>VB.net</li>
+            <li>AngularJs</li>
+            <li>React</li>
+            <li>MOSS2007</li>
+            <li>Java</li>
+            <li>C</li>
+            <li>C#</li>
+            <li>C++</li>
+            <li>MSSQL</li>
+            <li>MS Office</li>
+            <li>Symfony 6</li>
+            <li>HTML</li>
+            <li>XML</li>
+            <li>Typoscript</li>
+            <li>Jade</li>
+            <li>PL/SQL</li>
+            <li>JSON</li>
+            <li>CSS 3</li>
+            <li>Unix</li>
+            <li>C#.net</li>
+            <li>Linux</li>
+            <li>Github</li>
+        </ul>
+    </div>
+    <!-- End #skills -->
+
+    <div id="contact">
+        <h2>Get in Touch</h2>
+        <div id="contact-form">
+                <form action="<?php echo $_SERVER['SCRIPT_NAME'] ?>" method="POST" enctype="multipart/form-data">
+                <input type="hidden" name="_subject" value="Contact request from personal website" />
+                <input type="email" name="_replyto" placeholder="Your email" required>
+                <textarea name="message" placeholder="Your message" required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+        <!-- End #contact-form -->
+    </div>
+    <!-- End #contact -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-5 copyright">
+                    <p>
+                        Copyright &copy; <span id="current-year">2024</span> KIRAN GANGAVARAM
+                    </p>
+                </div>
+                <div class="col-sm-2 top">
+                    <span id="to-top">
+                        <i class="fa fa-chevron-up" aria-hidden="true"></i>
+                    </span>
+                </div>
+                <div class="col-sm-5 social">
+                    <ul>
+                        <li>
+                            <a href="https://github.com/xploreNew" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/kiran-mayi-gangavaram" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/scripts.min.js"></script>
+</body>
+
+</html>
